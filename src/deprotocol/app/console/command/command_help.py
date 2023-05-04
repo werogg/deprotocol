93500e10b601cb57f7e789bb8b0db49ba0ae0732
Joel Otero <supwer00@gmail.com>
2023-05-05 00:13:13 +0200

    Modify command help (#24)

--- src/deprotocol/app/console/command/command_help.py
+++ src/deprotocol/app/console/command/command_help.py
@@ -4,4 +4,8 @@ from deprotocol.app.logger import Logger
 
 class CommandHelp(Command):
     def handle_command(self, args=''):
-        Logger.get_logger().info("You can use x x and x")
+        Logger.get_logger().info("\nAvailable commands:\n"
+                                 "- connect <onion.address> - connect to a peer using their onion address\n"
+                                 "- connections - list all current connections\n"
+                                 "- quit - disconnect from all peers and quit DeProtocol\n"
+                                 "- message <connection> <message> - send a message to a specific connection")
