25103b7d095658de12f62d9c42f19a2707759e7c
Valentine <valiachernykh@yandex.ru>
2023-05-26 11:31:51 +0300

    adding asychronicity: Asynchronous connection Asynchronous console Asynchronous Pinger Asynchronous Node Connection Asynchronous tor download Asynchronous connection with tor controller
    A small change in architecture
    Some methods have been changed
    Fixed setup for Darwin system

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
 from application.app.application import DeProtocol
+from application.utils.portforwardlib import forwardPort
 
 if __name__ == '__main__':
     main_app = DeProtocol()
     asyncio.run(main_app.on_start())
+
